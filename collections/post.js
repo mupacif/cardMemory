20b4e9452ff5bfe3e8e8d152e5775cee3f4b136f
Pacifique <mupacif@gmail.com>
2016-04-25 15:40:24 +0200

    1st version

---
+++ collections/post.js
@@ -0,0 +1,2 @@
+
+post = new Mongo.Collection("studyjs");
---
+++ server/server.js
@@ -0,0 +1,21 @@
+
+var Hello=0;
+Meteor.onConnection(function(connection){
+  Hello++; 
+  console.log(Hello);
+});
+Meteor.publish("post", function()
+{
+return post.find();
+
+});
+
+ 
+Meteor.methods({
+'insertPost'(doc)
+{
+console.log("server methods"+doc);
+post.insert(doc);
+}
+
+});
